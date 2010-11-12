b7f9fee39ca8ae64302e590cdea12cecba53b4a6
raphaelcruzeiro@raphaelcruzeiro.com <raphaelcruzeiro@raphaelcruzeiro.com>
2010-11-12 16:06:43 -0200

    added missing files

---
+++ Web.Generics/Extensions/DatetimeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class DatetimeExtensions
+    {
+        /// <summary>
+        /// Converts the DateTime object to a RFC-822 compliant string.
+        /// </summary>
+        public static string ToRFC822Date(this DateTime dt)
+        {
+            string timeZone;
+            int offset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).Hours;
+            if (offset < 0)
+            {
+                offset *= -1;
+                timeZone = String.Format("-{0}", offset.ToString().PadLeft(2, '0'));
+            }
+            else
+                timeZone = String.Format("+{0}", offset.ToString().PadLeft(2, '0'));
+            return dt.ToString(String.Format("ddd, dd MMM yyyy HH:mm:ss {0}", timeZone.PadRight(5, '0')), System.Globalization.DateTimeFormatInfo.InvariantInfo);
+        }
+    }
+}
---
+++ Web.Generics/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Checks if the string is an e-mail.
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <returns></returns>
+        public static bool IsEmail(this string Email)
+        {
+            string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            Regex re = new Regex(strRegex);
+            if (re.IsMatch(Email))
+                return (true);
+            else
+                return (false);
+        }
+    }
+}
